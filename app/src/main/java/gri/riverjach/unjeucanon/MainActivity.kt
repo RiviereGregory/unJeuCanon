3dfd64331cd8962dbbd258118145b802ecdde989
RiviereGregory <gregory.riviere@laposte.net>
2020-11-14 16:01:24 +0100

    first commit

---
+++ app/src/main/java/gri/riverjach/unjeucanon/MainActivity.kt
@@ -0,0 +1,11 @@
+package gri.riverjach.unjeucanon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
